d6fcacac57c9a09e5e9636a3af2ed668ea262dc6
bhawkes3 <haw16018@byui.edu>
2018-06-05 09:20:46 -0600

    big update

---
+++ budget/script.js
@@ -0,0 +1,8 @@
+function newExpense(){
+    prompt('Please enter your amount');
+}
+
+function newItem(){
+    var name = prompt('Please enter the name of your new item');
+    var planned = prompt('please enter the amount you plan to spend this month');
+}
