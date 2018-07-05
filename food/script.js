f38da8763c35cfd6c5c1a03ca73c33e262b976ee
bhawkes3 <haw16018@byui.edu>
2018-07-05 10:47:20 -0600

    class update

--- food/script.js
+++ food/script.js
@@ -1,8 +1,17 @@
 //use fetch to retrieve an API
+function getFood(){
+    
+    fetch("http://food2fork.com/api/search?key={d611934e9c32166b791fbd39a3badfd2}&q=shredded%20chicken")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+}
 
-const foodAPI = {
-     
-};
+
+
 
 // http://food2fork.com/api/get THIS URL WILL GET RECIPES
 // api key: d611934e9c32166b791fbd39a3badfd2
