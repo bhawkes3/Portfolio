20f33bbbf06539bda3150dff9d8821327043543d
bhawkes3 <haw16018@byui.edu>
2018-07-17 11:11:04 -0600

    class update

--- food/script.js
+++ food/script.js
@@ -14,13 +14,44 @@ const baseUrl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/reci
 
 
 //go and get the information using the API
-  fetch("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?query=chicken%2C+onion%2C+garlic", myInit)
+  fetch("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?query=chicken%2C+broccoli", myInit)
   .then(function(response) {
+
+    console.log(response.results[0].title);
+    
+    let key = Object.keys(response);
+            // let nav = '<ul>';
+            // nav += "<li id = 'home'>Home</li>";
+            for (let i = 0, n = key.length; i < n; i++) {
+                let foodItem = key[i];
+               let box = document.createElement("div");
+               box.setAttribute("class" , "result-box");
+               let h2 = document.createElement("h2");
+               let pTime = document.createElement("p");
+               let servings = document.createElement("p");
+               let hText = document.createTextNode();
+               let timeText = document.createTextNode();
+               let servingText = document.createTextNode();
+               pTime.setAttribute("class" , "p-time");
+               servings.setAttribute("class" , "servings");
+               h2.appendChild(hText);
+               pTime.appendChild(timeText);
+               servings.appendChild(servingText);
+               box.appendChild(h2);
+               box.appendChild(pTime);
+               box.appendChild(servings);
+               let resultList = document.getElementById("result-list");
+               resultList.appendChild(box);
+
+            };
+            
+
     return response.json();
   })
   .then(function(myJson) {
     console.log(myJson);
   });
 }
+
 
 // "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C+gluten&number=10&offset=0&query=burger&type=main+course"
